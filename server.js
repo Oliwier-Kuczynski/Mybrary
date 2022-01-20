142d1b4d424dbfec948d3f142318f6d1deab520c
Oliwier <olikucz@vp.pl>
2022-01-20 19:32:27 +0100

    initial Author Routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
+const authorsRouter = require("./routes/authors");
 const mongoose = require("mongoose");
 const { response } = require("express");
 
@@ -9,11 +10,14 @@ app.set("view engine", "ejs");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
 
 mongoose.connect(
   "mongodb+srv://testing:Test1234@cluster0.adkv2.mongodb.net/database?retryWrites=true&w=majority"
 );
 
-app.use(indexRouter);
+app.use("/", indexRouter);
+app.use("/authors", authorsRouter);
 
 app.listen(process.env.PORT || 3000);
